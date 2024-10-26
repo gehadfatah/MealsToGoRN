bca45eb02db09b983f604771e49da329fbd14fd3
gehad.abdelfatah <gehad.abdelfatah45@gmail.com>
2024-10-27 01:26:36 +0300

    fix header and fix fu package.json

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -13,18 +13,21 @@ const RestaurantStack = createStackNavigator();
 export const RestaurantsNavigator = () => {
   return (
     <RestaurantStack.Navigator
-      headerMode="none"
+
       screenOptions={{
         ...TransitionPresets.ModalPresentationIOS,
+        headerShown:false
       }}
     >
       <RestaurantStack.Screen
         name="Restaurants"
         component={RestaurantsScreen}
+
       />
       <RestaurantStack.Screen
         name="RestaurantDetail"
         component={RestaurantDetailScreen}
+
       />
     </RestaurantStack.Navigator>
   );
