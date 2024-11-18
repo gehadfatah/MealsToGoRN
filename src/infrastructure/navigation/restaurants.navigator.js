3c55436f3b6806c6dc5c9af9385022979db19844
gehad.abdelfatah <gehad.abdelfatah45@gmail.com>
2024-11-18 10:29:37 +0200

    success with updated dependences:)

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -16,6 +16,7 @@ export const RestaurantsNavigator = () => {
         headerShown={false}
       screenOptions={{
         ...TransitionPresets.ModalPresentationIOS,
+        headerShown: false
       }}
     >
       <RestaurantStack.Screen
